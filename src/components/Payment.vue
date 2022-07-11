8541c113eb29adcece18527af85142d0b3e6bcff
Koichi.Shimomura <unieye51@gmail.com>
2022-07-11 10:59:00 +0900

    4-23 RefとReactive

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
-const itemName1 = 'Desk'
+import { ref, reactive } from 'vue'
+
+// refでリアクティブに値を参照する（単一の値をリアクティブにする）
+// const itemName1 = ref<string>('Desk')
 const itemName2 = 'Bike'
 
-const price1 = 40000
+// オブジェクト全体をリアクティブにする
+const item1 = reactive({
+	name: 'Desk',
+	price: 40000
+})
+
+// const price1 = 40000
 const price2 = 20000
 
 const url1 = 'https://www.amazon.co.jp/dp/B09G9K89MD'
@@ -11,8 +20,13 @@ const buy = (itemName: string) => {
 	alert('Are you sure to buy ' + itemName + '?')
 }
 
-const input = (event: any) => {
-	console.log('event.target.value:', event.target.value)
+const inputName = (event: any) => {
+	// itemName1.value = event.target.value // ref形式の場合
+	item1.name = event.target.value
+}
+
+const inputPrice = (event: any) => {
+	item1.price = event.target.value
 }
 
 </script>
@@ -20,12 +34,13 @@ const input = (event: any) => {
 <template>
 	<div class="container">
 		<h1>Payment</h1>
-		<input v-on:input="input" />
+		<input v-on:input="inputName" />
+		<input v-on:input="inputPrice" />
 		<div class="payment">
-			<label>{{ itemName1 }}</label>
-			<label>{{ price1 }}円</label>
+			<label>{{ item1.name }}</label>
+			<label>{{ item1.price }}円</label>
 			<a v-bind:href="url1">bought at...</a>
-			<button v-on:click="buy(itemName1)">BUY</button>
+			<button v-on:click="buy(item1.name)">BUY</button>
 		</div>
 		<div class="payment">
 			<label>{{ itemName2 }}</label>
@@ -49,6 +64,10 @@ const input = (event: any) => {
 	height: 80px;
 	width: 400px;
 	background-color: aliceblue;
+	margin-bottom: 8px;
+}
+
+input {
 	margin-bottom: 8px;
 }
 
